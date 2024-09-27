13f2b10740e447914688f17bb30c88752e5e58f0
Kevin Gonzalez <kejogo.dev@outlook.com>
2024-09-26 22:25:01 -0500

    feat: 11 querys

--- Proyecto_historiaSQL/dql.sql
+++ Proyecto_historiaSQL/dql.sql
@@ -1,3 +1,4 @@
+-- 10 EVENTOS HISTORICOS DETALLANDO NOMBRE, DESCRIPCION, LOCALIDADES, CAUSAS, ALCANCE, TIPO DE CONFLICO, ASPECTOS QUE TRANSFORMO, MOMENTOS IMPORTANTES, QUÉ DOCUMENTACION EXISTE, PERSONAJES EMBLEMATICOS Y LA DECADA EN LA QUE INICIO DICHO EVENTO
 SELECT
     e.id_Evento,
     e.Nombre,
@@ -30,3 +31,156 @@ FROM
     LEFT JOIN Personajes p ON ep.id_Personajes = p.id_Personajes
 GROUP BY
     e.id_Evento
+    -- LIMIT 10
+
+-- Personajes nacidos entre la decada de 1500s y 1800s que hayan contribuido en algun evento transformador en el area Juridica y su edad de fallecimiento
+SELECT
+    p.id_Personajes,
+    p.Nombre,
+    p.Fecha_Nacimiento,
+    (p.Fecha_Fallecimiento - p.Fecha_Nacimiento) AS Edad_de_Fallecimiento
+FROM
+    Personajes p
+    JOIN Eventos_Personajes ep ON p.id_Personajes = ep.id_Personajes
+    JOIN Eventos e ON ep.id_Evento = e.id_Evento
+    JOIN Eventos_AspectoTransformado eat ON e.id_Evento = eat.id_Evento
+    JOIN AspectoTransformado at ON eat.id_AspectoTransformado = at.id_AspectoTransformado
+WHERE
+    p.Fecha_Nacimiento BETWEEN 1500 AND 1800
+    AND at.Aspecto = 'Jurídico';
+
+-- ¿Existe documentacion Cartografica sobre la llegada del hombre a la luna?
+SELECT
+    CASE 
+        WHEN EXISTS (
+            SELECT 1
+            FROM Eventos e
+            JOIN Eventos_Documentacion ed ON e.id_Evento = ed.id_Evento
+            JOIN Documentacion d ON ed.id_Documentacion = d.id_Documentacion
+            WHERE e.Nombre = "Llegada del hombre a la Luna" AND d.Nombre = "Cartográfica"
+        ) THEN "Verdadero"
+        ELSE "Falso"
+    END AS "¿Existe documentación cartográfica sobre la llegada del hombre a la luna?"
+
+-- ¿Cual es el tipo de conflico mas comun?
+SELECT
+    Tc.Nombre AS "Tipo de conflicto",
+    COUNT(*) AS "Coincidencias"
+FROM
+    Eventos e
+    JOIN TipoConflicto Tc ON e.id_TipoConflicto = Tc.id_TipoConflicto
+GROUP BY
+    Tc.Nombre
+ORDER BY
+    COUNT(*) DESC
+
+-- Mostrar unicamente los Eventos historicos con 4 o más personajes implicados
+SELECT
+    e.id_Evento,
+    e.Nombre,
+    COUNT(*) AS "# Personajes"
+FROM
+    Eventos e
+    JOIN Eventos_Personajes ep ON e.id_Evento = ep.id_Evento
+    JOIN Personajes p ON ep.id_Personajes = p.id_Personajes
+GROUP BY
+    e.id_Evento
+HAVING
+    COUNT(*) >= 4
+ORDER BY
+    COUNT(*) DESC
+
+-- Personajes que a dia de hoy no hayan fallecido o que lleven menos de 2 decadas fallecidos
+SELECT
+    id_Personajes,
+    Nombre,
+    Fecha_Nacimiento
+FROM
+    Personajes
+WHERE
+    Fecha_Fallecimiento IS NULL
+    OR (YEAR(CURDATE()) - Fecha_Fallecimiento) < 20
+ORDER BY        
+    Fecha_Nacimiento ASC
+
+-- Eventos historicos de caracter global o mundial
+SELECT
+    e.id_Evento,
+    e.Nombre,
+    CASE
+        WHEN tc.Nombre = "Guerra Mundial" THEN CONCAT("Tipo De Conflicto: ", tc.Nombre)
+        WHEN a.Tipo = "Global" THEN CONCAT("Alcance: ", a.Tipo)
+    END AS "Caracter"
+FROM
+    Eventos e
+    JOIN TipoConflicto tc ON e.id_TipoConflicto = tc.id_TipoConflicto
+    JOIN Alcance a ON e.id_Alcance = a.id_Alcance
+WHERE
+    tc.Nombre = "Guerra Mundial" OR a.Tipo = "Global"
+
+-- Momentos emblematicos con sus eventos historicos ocurridos antes del siglo XVII con su fecha exacta de ejecución
+SELECT
+    s.id_SubEventos,
+    s.Nombre AS "Momento Emblematico",
+    e.Nombre AS "Evento",
+    s.Fecha
+FROM
+    SubEventos s
+    JOIN Eventos_SubEventos es ON s.id_SubEventos = es.id_SubEventos
+    JOIN Eventos e ON es.id_Evento = e.id_Evento
+WHERE
+    YEAR(Fecha) < 1600
+ORDER BY
+    Fecha ASC
+
+-- SubEventos ocurridos en el 3er trimestre de 1789 y su detalle
+SELECT
+    id_SubEventos,
+    Nombre,
+    Fecha,
+    Descripcion
+FROM
+    SubEventos
+WHERE
+    YEAR(Fecha) = 1789 AND
+    MONTH(Fecha) BETWEEN 07 AND 09
+
+-- ¿Cúal es el evento mas reciente?
+SELECT
+    e.id_Evento,
+    e.Nombre,
+    d.Decada
+FROM
+    Eventos e
+    JOIN Decada d ON e.id_Decada = d.id_Decada
+ORDER BY
+    ABS(CAST(LEFT(d.Decada, 4) AS SIGNED) - YEAR(CURDATE())) ASC
+LIMIT 1
+
+-- Eventos historicos que desatarón un conflico internacional por motivos Ideologicos en el presente milenio
+SELECT
+    e.id_Evento,
+    e.Nombre,
+    d.Decada
+FROM
+    Eventos e
+    JOIN TipoConflicto tc ON e.id_TipoConflicto = tc.id_TipoConflicto
+    JOIN Alcance a ON e.id_Alcance = a.id_Alcance
+    JOIN Decada d ON e.id_Decada = d.id_Decada
+WHERE
+    tc.Nombre = "Conflicto Internacional"
+    AND a.Tipo = "Ideológico"
+
+-- Eventos historicos sucedidos entre la decada de 1910 y 1930 que hayan transformado al mundo economicamente
+SELECT
+    e.id_Evento,
+    e.Nombre,
+    d.Decada
+FROM
+    Eventos e
+    JOIN Decada d ON e.id_Decada = d.id_Decada
+    JOIN Eventos_AspectoTransformado eat ON e.id_Evento = eat.id_Evento
+    JOIN AspectoTransformado at ON eat.id_AspectoTransformado = at.id_AspectoTransformado
+WHERE
+    at.Aspecto = "Económico"
+    AND CAST(LEFT(d.Decada, 4) AS UNSIGNED) BETWEEN 1910 AND 1930
